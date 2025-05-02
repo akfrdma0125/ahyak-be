7d64a297d7eadff5b1daaf41022c45632a8a23d1
akfrdma0125 <nogain1215@gmail.com>
2025-05-02 14:39:32 +0900

    1. feat: 추가 약 등록(약 자체 등록)

--- routes/medicine.js
+++ routes/medicine.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getMedicinesByFilter } = require("../infra/model/medicine");
+const { getMedicinesByFilter, createMedicine } = require("../infra/model/medicine");
 
 router.get("/", async (req, res) => {
     try {
@@ -9,6 +9,38 @@ router.get("/", async (req, res) => {
         res.json({medicine: medicines});
     } catch (err) {
         console.error("약 검색 오류:", err);
+        res.status(500).json({ message: "서버 오류" });
+    }
+});
+
+// 자유 약 등록
+router.post("/", async (req, res) => {
+    try {
+        const { name, print, shape, color, type, line} = req.body;
+
+        if (!name) {
+            res.status(400).json({ message: "Bad Request" });
+            return;
+        }
+
+        // 자유 약 등록 시 기본값 설정
+        const seq = "0"; // 기본값으로 설정
+        const tokenized = ""; // 기본값으로 설정
+
+        const newMedicine = await createMedicine({
+            seq,
+            name, // 약 이름
+            print, // 식별 문자
+            shape, // 약 모양
+            color, // 약 색상
+            type, // 약 제형
+            line, // 분할선
+            tokenized
+        });
+
+        res.json({ status: "success", data: newMedicine });
+    } catch (err) {
+        console.error("약 생성 오류:", err);
         res.status(500).json({ message: "서버 오류" });
     }
 });
